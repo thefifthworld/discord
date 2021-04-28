b20e97014b6d5cc9ffb92b0c14273c71ee25724b
Jason Godesky <jefgodesky@gmail.com>
2021-04-27 20:30:15 -0400

    Character sheet: Describe missing body parts

--- embeds/sheet-char.js
+++ embeds/sheet-char.js
@@ -1,7 +1,94 @@
 const Discord = require('discord.js')
 const { colors, domain } = require('../config')
-const { cyclicPrinciples } = require('../data')
-const { formatDate } = require('../utils')
+const { cyclicPrinciples, bodyPartsPlural, allFingers, allToes } = require('../data')
+const { list, except, formatDate } = require('../utils')
+
+/**
+ * Describe the condition of a limb. If describing an arm that's missing, say
+ * that the arm is missing. If the arm is present but the hand is missing, say
+ * that the hand is missing. If the arm and the hand are present, describe
+ * which fingers (if any) may be missing.
+ * @param {Object} body - The character's body object.
+ * @param {string} side - `Left` or `Right`
+ * @param {string} type - `arm` or `leg`
+ * @returns {string[]} - An array of strings describing what parts of the limb
+ *   might be missing.
+ */
+
+const describeMissingLimb = (body, side, type) => {
+  const arr = []
+  const s = side.toLowerCase()
+  const extremity = type === 'arm' ? 'hand' : 'leg' ? 'foot' : null
+  const digits = type === 'arm' ? 'finger' : 'leg' ? 'toe' : null
+  const full = type === 'arm' ? allFingers : 'leg' ? allToes : null
+
+  if (extremity && digits && full) {
+    if (body[bodyPartsPlural[type]][s] === 'Missing') {
+      arr.push(`${side} ${type} missing`)
+    } else if (body[bodyPartsPlural[extremity]][s] === 'Missing') {
+      arr.push(`${side} ${extremity} missing`)
+    } else {
+      const missingDigits = except(full, body[bodyPartsPlural[digits]][s]).filter(f => f !== 'Thumb').map(f => `${f.toLowerCase()} ${digits}`)
+      if (digits === 'finger' && !body[bodyPartsPlural[digits]][s].includes('Thumb')) missingDigits.unshift('thumb')
+      if (missingDigits.length > 0) arr.push(`Missing ${list(missingDigits)} on ${s} ${extremity}`)
+    }
+  }
+
+  return arr
+}
+
+/**
+ * Describe which body parts a character is missing.
+ * @param {Object} body - The character's body object.
+ * @returns {string[]} - An array of strings describing the body parts that a
+ *   character is missing.
+ */
+
+const describeMissingBodyParts = body => {
+  let missing = []
+
+  if (body.eyes.left === 'Blind' && body.eyes.right === 'Blind') {
+    missing.push('Blind')
+  } else if (body.eyes.left === 'Blind') {
+    missing.push('Blind in left eye')
+  } else if (body.eyes.right === 'Blind') {
+    missing.push('Blind in right eye')
+  }
+
+  if (body.eyes.left === 'Missing' && body.eyes.right === 'Missing') {
+    missing.push('Missing both eyes')
+  } else if (body.eyes.left === 'Missing') {
+    missing.push('Missing left eye')
+  } else if (body.eyes.right === 'Missing') {
+    missing.push('Missing right eye')
+  }
+
+  if (body.ears.left === 'Deaf' && body.ears.right === 'Deaf') {
+    missing.push('Deaf')
+  } else if (body.ears.left === 'Deaf') {
+    missing.push('Deaf in left ear')
+  } else if (body.ears.right === 'Deaf') {
+    missing.push('Deaf in right ear')
+  }
+
+  if (body.ears.left === 'Missing' && body.ears.right === 'Missing') {
+    missing.push('Missing both ears')
+  } else if (body.ears.left === 'Missing') {
+    missing.push('Missing left ear')
+  } else if (body.ears.right === 'Missing') {
+    missing.push('Missing right ear')
+  }
+
+  missing = [
+    ...missing,
+    ...describeMissingLimb(body, 'Right', 'arm'),
+    ...describeMissingLimb(body, 'Left', 'arm'),
+    ...describeMissingLimb(body, 'Right', 'leg'),
+    ...describeMissingLimb(body, 'Left', 'leg')
+  ]
+
+  return missing
+}
 
 /**
  * Describe the current state of a character's injuries.
@@ -14,7 +101,7 @@ const describeInjuries = body => {
   if (!body.scars) body.scars = []
   const injuryTypes = [ 'Exhaustion', 'Bruises', 'Cuts', 'Wounds' ]
   const injuries = injuryTypes.map(type => `${body[type.toLowerCase()] && body[type.toLowerCase()] === true ? '●' : '○'} ${type}`)
-  const desc = [ injuries.join(' / '), ...body.scars ]
+  const desc = [ injuries.join(' / '), ...describeMissingBodyParts(body) ]
   return desc.join('\n')
 }
 
