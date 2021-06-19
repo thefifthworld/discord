6d4ec05bdd450c4772bed78284f915c2bfc56a53
Jason Godesky <jefgodesky@gmail.com>
2021-06-18 21:50:48 -0400

    Fix broken guard rails around paying awareness

--- pay.js
+++ pay.js
@@ -17,13 +17,11 @@ const pay = async (character, place, tale) => {
   if (character && character.awareness && character.awareness > 0) {
     character.awareness--
     if (place && place.awareness) place.awareness++
-
-    if (character.sheet) await character.sheet.edit({embed: charSheet(character)})
-    if (place.sheet) await place.sheet.edit({embed: placeSheet(place)})
-    if (tale.summary) await tale.summary.edit({embed: taleSummary(tale)})
-  } else if (character && character.awareness) {
-    const players = getPlayers(tale)
-    const filtered = players.filter(p => p.character && p.character.path === character.path)
+    if (character.sheet) await character.sheet.edit({ embed: charSheet(character) })
+    if (place.sheet) await place.sheet.edit({ embed: placeSheet(place) })
+    if (tale.summary) await tale.summary.edit({ embed: taleSummary(tale) })
+  } else if (character && typeof character.awareness === 'number') {
+    const filtered = tale.players.filter(p => p.character && p.character.path === character.path)
     const player = filtered.length > 0 ? filtered[0] : null
     const msg = player ? { content: `${mention(player)},`, embed: outOfAwareness() } : { embed: outOfAwareness() }
     await tale.channel.send(msg)
